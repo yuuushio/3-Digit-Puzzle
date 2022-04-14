4ff2375c44676143541f4b47bfd10aae482c77b6
yuuushio <>
2022-04-14 22:35:59 +1000

    test gen_ith_children method for index 1 and 2

--- three_digit_test.py
+++ three_digit_test.py
@@ -18,4 +18,18 @@ def test_zero_th_nine_value():
     a = t.gen_ith_children(node, 0)
     assert a.[0].value == 820
 
+def test_gen_first_index():
+    node = t.Node(320)
+    a = t.gen_ith_children(node, 1)
+    assert (a[0].value, a[1].value) == (310, 330)
 
+def test_gen_third_digit():
+    node = t.Node(320)
+    a = gen_ith_children(node, 2)
+    # also tests "cant subtract from 0" functionality
+    assert len(a) = 1
+
+def test_gen_third_digit_value():
+    node = t.Node(320)
+    a = gen_ith_children(node, 2)
+    assert (a[0].value) = (321)
