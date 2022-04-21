f5fa1eca316affd6b94d73737bec8f7ce17fc44f
yuuushio <>
2022-04-22 06:28:21 +1000

    use fstrings and refactorize children method

--- three_dig.py
+++ three_dig.py
@@ -16,10 +16,8 @@ class Node:
         return set(self.children) == set(other.children) and self.value == other.value
 
 def mh_heuristic(a, b):
-    str_a = str(a)
-    str_b = str(b)
     res = 0
-    for x,y in zip(str_a, str_b):
+    for x,y in zip(str(a), str(b)):
         res += math.fabs(int(x)-int(y))
 
     return res
@@ -42,75 +40,81 @@ def gen_ith_children(node, i):
         return []
     if dig_add is not None and dig_sub is not None:
         if i == 0:
-            list_a = [str(dig_add), str(node.value)[1], str(node.value)[2]]
-            list_b = [str(dig_sub), str(node.value)[1], str(node.value)[2]]
+            str_a = f"{str(dig_add)}{str(node.value)[1]}{str(node.value)[2]}"
+            str_b = f"{str(dig_sub)}{str(node.value)[1]}{str(node.value)[2]}"
         elif i == 1:
-            list_a = [str(node.value)[0], str(dig_add), str(node.value)[2]]
-            list_b = [str(node.value)[0], str(dig_sub), str(node.value)[2]] 
+            str_a = f"{str(node.value)[0]}{str(dig_add)}{str(node.value)[2]}"
+            str_b = f"{str(node.value)[0]}{str(dig_sub)}{str(node.value)[2]}"
         else:
-            list_a = [str(node.value)[0], str(node.value)[1], str(dig_add)]
-            list_b = [str(node.value)[0], str(node.value)[1], str(dig_sub)]
+            str_a = f"{str(node.value)[0]}{str(node.value)[1]}{str(dig_add)}"
+            str_b = f"{str(node.value)[0]}{str(node.value)[1]}{str(dig_sub)}"
         
-        str_a = str_b = ""
-        for c in list_a: str_a += c
-        for c in list_b: str_b += c
         return [Node(int(str_a), parent=node), Node(int(str_b), parent=node)]
 
     elif dig_add: 
         if i == 0:
-            list_a = [str(dig_add), str(node.value)[1], str(node.value)[2]]
+            str_a = f"{str(dig_add)}{str(node.value)[1]}{str(node.value)[2]}"
         elif i == 1:
-            list_a = [str(node.value)[0], str(dig_add), str(node.value)[2]]
+            str_a = f"{str(node.value)[0]}{str(dig_add)}{str(node.value)[2]}"
         else:
-            list_a = [str(node.value)[0], str(node.value)[1], str(dig_add)]
+            str_a = f"{str(node.value)[0]}{str(node.value)[1]}{str(dig_add)}"
 
-        str_a = ""
-        for c in list_a: str_a += c
         return [Node(int(str_a), parent=node)]
     else:
         if i == 0:
-            list_b = [str(dig_sub), str(node.value)[1], str(node.value)[2]]
+            str_b = f"{str(dig_sub)}{str(node.value)[1]}{str(node.value)[2]}"
         elif i == 1:
-            list_b = [str(node.value)[0], str(dig_sub), str(node.value)[2]] 
+            str_b = f"{str(node.value)[0]}{str(dig_sub)}{str(node.value)[2]}"
         else:
-            list_b = [str(node.value)[0], str(node.value)[1], str(dig_sub)]
+            str_b = f"{str(node.value)[0]}{str(node.value)[1]}{str(dig_sub)}"
 
-        str_b = "" 
-        for c in list_b: str_b += c
         return [Node(int(str_b), parent=node)]
+
+# Returns a new list that doesnt consist of forbidden nodes
+def renew_list(node_li, f):
+    new_list = []
+    if f is not None and len(f) != 0:
+        for n in node_li:
+            if n.value not in f:
+                new_list.append(n)
+        return new_list
+    else:
+        return node_li
+
+# Generates the nodes' children
+def node_children_list_helper(node, renewed_li):
+    # Children added as raw values - easier for comparisons
+    for n in renewed_li: node.children.append(n.value)
 
 def gen_children(node, forbidden):
     if node.parent is None:
         node_li = []
         # generate all children since no parent to depend on
         for i in range(0, 3):
-            node_li += gen_ith_children(node, i)
-        
-        # children added as raw values - for easier comparisons of equality
-        #  using set/dict
-        for n in node_li:
-            node.children.append(n.value)
-        return node_li
+            node_li += gen_ith_children(node, i) 
+        renewed_li = renewed_list(node_li, forbidden)
+        node_children_list_helper(node, renewed_list)
+        return renewed_list 
     else:
         diff = math.fabs(node.parent.value - node.value)
     if diff == 100:
         node_li = gen_ith_children(node, 1) + gen_ith_children(node, 2)
-        for n in node_li:
-            node.children.append(n.value)
-        return node_li
+        renewed_li = renewed_list(node_li, forbidden)
+        node_children_list_helper(node, renewed_list)
+        return renewed_list 
 
     elif diff == 10:
         node_li = gen_ith_children(node, 0) + gen_ith_children(node, 2)
-        for n in node_li:
-            node.children.append(n.value)
-        return node_li
+        renewed_li = renewed_list(node_li, forbidden)
+        node_children_list_helper(node, renewed_list)
+        return renewed_list 
 
     else:
         # assume 3rd digit was changed (good assumption if valid input)
         node_li = gen_ith_children(node, 0) + gen_ith_children(node, 1)
-        for n in node_li:
-            node.children.append(n.value)
-        return node_li
+        renewed_li = renewed_list(node_li, forbidden)
+        node_children_list_helper(node, renewed_list)
+        return renewed_list 
 
 def get_path(node):
     n = node
@@ -121,6 +125,13 @@ def get_path(node):
         # Trace back through the parents for path
         n = n.parent
     return path[::-1] # Return the reverse of the list since we tracked back
+
+# Generates the output path and expanded list
+def generate_return_val(expanded):
+    final_expanded = []
+    for n in expanded: final_expanded.append(n.value)
+    path = get_path(expanded[-1])
+    return path, final_expanded
 
 # Can implement bfs and dfs in the same method, since the only
 # difference is the way the children are added to the fringe.
@@ -151,14 +162,9 @@ def bfs_dfs(start, goal, forbidden=None, bfs=True):
         current = tmp
 
     if len(expanded) == 1000: return "No solution found, limit reached."
-
     # If we're here, means goal has been found
     expanded.append(current)
-    final_expanded = []
-    for n in expanded: final_expanded.append(n.value)
-    path = get_path(current)
-
-    return path, final_expanded
+    return generate_return_val(expanded)
 
 # DFS that only gets expanded up to a certain depth
 # Return: [] -> expanded nodes, true if goal found, else false
@@ -189,9 +195,7 @@ def ids_helper(s, g, f, d, expanded):
             visited_nodes.append(tmp)
             if tmp is not None:
                 current = tmp
-    if len(expanded) == 1000:
-        return expanded, False
-
+    if len(expanded) == 1000: return expanded, False
     expanded.append(current)
     return expanded, True
 
@@ -213,11 +217,9 @@ def ids(start, goal, forbidden=None):
             expanded = dls[0]
         if dls[1] == False and len(expanded) >= 1000:
             return "No solution found. Limit Reached"
-        final_expanded = []
-        # Since expanded is a list of node - get their values
-        for n in expanded:
-            final_expanded.append(n.value)
-        return final_expanded
+
+        return generate_return_val(expanded)
+
 
 def greedy(s, g, f=None):
     expanded = []
@@ -244,14 +246,9 @@ def greedy(s, g, f=None):
         current = tmp
         
     if len(expanded) == 1000: return "No solution found, limit reached."
-
     # If we're here, means goal has been found
     expanded.append(current)
-    final_expanded = []
-    for n in expanded: final_expanded.append(n.value)
-    path = get_path(current)
-
-    return path, final_expanded
+    return generate_return_val(expanded)
 
 # Returns f; f = g + h
 # where g = cost so far to reach node
@@ -277,20 +274,17 @@ def a_star(s,g,f=None):
             fringe.put((h, i, c))
             i -= 1
         # Get next node to expand from fringe
-        if len(fringe) != 0:
+        if not fringe.empty() != 0:
             tmp = fringe.get()[2]
-        while tmp in visited and len(fringe) != 0:
+        while tmp in visited and not fringe.empty() != 0:
             # Keep popping from fringe till an un-visited node is found
             tmp = fringe.get()[2]
         visited.append(tmp)
         current = tmp
 
     if len(expanded) == 1000: return "No solution found."
-
     # Goal has been found: get path and the list of expanded nodes
     expanded.append(current)
-    final_expanded = []
-    for n in expanded: final_expanded.append(n.value)
-    path = get_path(current)
+    return generate_return_val(expanded)
 
-    return path, final_expanded
+print(a_star(Node(320), Node(110)))
